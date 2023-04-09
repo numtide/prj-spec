9be1767781a32563e4f29f99a890aae2b35ce19e
David Arnold <david.arnold@iohk.io>
2023-04-08 21:03:24 -0500

    contrib(golang): add env setters and file getters

--- contrib/go/env.go
+++ contrib/go/env.go
@@ -110,3 +110,72 @@ func prjRoot() string {
 	}
 	return p
 }
+// SetAll sets all environment variables in the current environment
+func SetAll() {
+    Reload()
+    os.Setenv("PRJ_ROOT", PrjRoot)
+    os.Setenv("PRJ_ID", PrjId)
+    os.Setenv("PRJ_CONFIG_HOME", PrjConfigHome)
+    os.Setenv("PRJ_RUNTIME_DIR", PrjRuntimeDir)
+    os.Setenv("PRJ_CACHE_HOME", PrjCacheHome)
+    os.Setenv("PRJ_DATA_HOME", PrjDataHome)
+    os.Setenv("PRJ_STATE_HOME", PrjStateHome)
+    os.Setenv("PRJ_PATH", PrjPath)
+}
+
+// DataFile returns a suitable location for the specified data file.
+// The relPath parameter must contain the name of the data file, and
+// optionally, a set of parent directories (e.g. appname/app.data).
+// If the specified directories do not exist, they will be created relative
+// to the base data directory. On failure, an error containing the
+// attempted paths is returned.
+func DataFile(relPath string) (string, error) {
+    	Reload()
+	return pathutil.Create(relPath, []string{PrjDataHome})
+}
+
+// ConfigFile returns a suitable location for the specified config file.
+// The relPath parameter must contain the name of the config file, and
+// optionally, a set of parent directories (e.g. appname/app.yaml).
+// If the specified directories do not exist, they will be created relative
+// to the base config directory. On failure, an error containing the
+// attempted paths is returned.
+func ConfigFile(relPath string) (string, error) {
+    	Reload()
+	return pathutil.Create(relPath, []string{PrjConfigHome})
+}
+
+// StateFile returns a suitable location for the specified state file. State
+// files are usually volatile data files, not suitable to be stored relative
+// to the $XDG_DATA_HOME directory.
+// The relPath parameter must contain the name of the state file, and
+// optionally, a set of parent directories (e.g. appname/app.state).
+// If the specified directories do not exist, they will be created relative
+// to the base state directory. On failure, an error containing the
+// attempted paths is returned.
+func StateFile(relPath string) (string, error) {
+    	Reload()
+	return pathutil.Create(relPath, []string{PrjStateHome})
+}
+
+// CacheFile returns a suitable location for the specified cache file.
+// The relPath parameter must contain the name of the cache file, and
+// optionally, a set of parent directories (e.g. appname/app.cache).
+// If the specified directories do not exist, they will be created relative
+// to the base cache directory. On failure, an error containing the
+// attempted paths is returned.
+func CacheFile(relPath string) (string, error) {
+    	Reload()
+	return pathutil.Create(relPath, []string{PrjCacheHome})
+}
+
+// RuntimeFile returns a suitable location for the specified runtime file.
+// The relPath parameter must contain the name of the runtime file, and
+// optionally, a set of parent directories (e.g. appname/app.pid).
+// If the specified directories do not exist, they will be created relative
+// to the base runtime directory. On failure, an error containing the
+// attempted paths is returned.
+func RuntimeFile(relPath string) (string, error) {
+    	Reload()
+	return pathutil.Create(relPath, []string{PrjRuntimeDir})
+}
